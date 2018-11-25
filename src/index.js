0f948054c6f82d328223f3d5953edf8fe9c91980
marcingal <marcin921125@gmail.com>
2018-11-25 17:31:52 +0100

    working on history of moves

--- src/index.js
+++ src/index.js
@@ -14,27 +14,7 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         squares: Array(9).fill(null),
-    //         xIsNext: true,
-    //     }
-    // }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice()
-        if (calculateWinner(squares) || squares[i]) {
-            return
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O'
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        })
-    }
-
-    renderSquare(i) {
+   renderSquare(i) {
         return (
             <Square
                 value={this.props.squares[i]}
@@ -44,17 +24,16 @@ class Board extends React.Component {
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares)
-        let status
-        if (winner) {
-            status = 'Winner: ' + winner
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-        }
+        // const winner = calculateWinner(this.state.squares)
+        // let status
+        // if (winner) {
+        //     status = 'Winner: ' + winner
+        // } else {
+        //     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        // }
 
         return (
             <div>
-                <div className={status}>{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -76,23 +55,40 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-constructor(props) {
-    super(props)
-    this.state = {
-        history: [{
-            squares: Array(9).fill(null),
-        }],
-        xIsNext: true,
+    constructor(props) {
+        super(props)
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
 
+        }
     }
-}
+
+    
+    handleClick(i) {
+        const history = this.state.history
+        const current = history[history.length - 1]
+        const squares = current.squares.slice()
+        if (calculateWinner(squares) || squares[i]) {
+            return
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            history: history.concat([{
+            squares: squares,
+        }]),
+            xIsNext: !this.state.xIsNext,
+        })
+    }
 
     render() {
-const history = this.state.history
-const current = history[history.length - 1]
-const winner = calculateWinner(current.squares)
+        const history = this.state.history
+        const current = history[history.length - 1]
+        const winner = calculateWinner(current.squares)
 
-let status
+        let status
         if (winner) {
             status = 'Winner: ' + winner
         } else {
@@ -104,12 +100,13 @@ let status
             <div className="game">
                 <div className="game-board">
                     <Board
-                    squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
                     />
+
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
@@ -141,33 +138,3 @@ function calculateWinner(squares) {
     }
     return null
 }
-
-/*
-history = [
-    //Before first move
-    {
-        squares: [
-            null, null, null,
-            null, null, null,
-            null, null, null,
-        ]
-    },
-    //After first move
-    {
-        squares: [
-            null, null, null,
-            null, null, null,
-            null, null, null,
-        ]
-    },
-    //After second move
-    {
-        squares: [
-            null, null, null,
-            null, null, null,
-            null, null, null,
-        ]
-    },
-    // ...
-]
-*/
